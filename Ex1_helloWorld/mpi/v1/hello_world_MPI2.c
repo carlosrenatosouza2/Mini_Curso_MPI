d3f0e6c89cfd6cf946dd02cb003a05e6a3d3396b
renato <carlos.renato.souza@gmail.com>
2023-08-22 18:08:18 -0300

    mpiv1.2

--- Ex1_helloWorld/mpi/v1/hello_world_MPI2.c
+++ Ex1_helloWorld/mpi/v1/hello_world_MPI2.c
@@ -5,13 +5,19 @@ int main(int argc, char const *argv[])
 {
     int r, p;
 
+    // Inicializa o MPI: inicio da regiao paralela.
     MPI_Init(NULL, NULL);
 
+    // Qual processo sou eu? 
+    // Consulta o numero do meu processo:
     MPI_Comm_rank(MPI_COMM_WORLD, &r);
+
+    // Consulta quantos processos existem no total:
     MPI_Comm_size(MPI_COMM_WORLD, &p);
 
     printf("\n Hello World from rank %d of %d processes.\n", r, p); 
 
+    // Finaliza o MPI: fecha a regiao paralela.
     MPI_Finalize();
 
     return 0;
