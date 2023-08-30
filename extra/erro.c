4614583819df7ecfb800906c8e645877e49237f1
carlosrenatosouza2 <carlos.renato.souz@gmail.com>
2023-08-30 13:51:51 -0300

    add extra ex. erro num.

---
+++ extra/erro.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main( int argc, char *argv[])
+{
+   int k, n;
+   float x, s, se;
+
+   //n = 100000;
+   //x = 0.1;
+   s = 0.0;
+   n = 80000;
+   x = 0.125;
+
+   for (k = 0; k < n; k++)
+      s += x;
+   
+   se = x * n;
+
+   printf("n = %d\n", n);
+   printf("x = %f\n", x);
+   printf("Se = %f\n", se);
+   printf("S  = %f\n", s);
+   
+   return 0;
+}
